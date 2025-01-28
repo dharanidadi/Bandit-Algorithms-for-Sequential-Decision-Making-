1e9f7aec4b5cc444b242dc924d495f53bb5c4e67
dharanidadi <85421975+dharanidadi@users.noreply.github.com>
2025-01-29 02:00:57 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,58 @@
+"""
+NOTE: You are only allowed to edit this file between the lines that say:
+    # START EDITING HERE
+    # END EDITING HERE
+
+This file contains the MultiBanditsAlgo class. Here are the method details:
+    - __init__(self, num_arms, horizon): This method is called when the class
+        is instantiated. Here, you can add any other member variables that you
+        need in your algorithm.
+    
+    - give_pull(self): This method is called when the algorithm needs to
+        select an arm to pull. The method should return the index of the arm
+        that it wants to pull (0-indexed).
+    
+    - get_reward(self, arm_index, set_pulled, reward): This method is called 
+        just after the give_pull method. The method should update the 
+        algorithm's internal state based on the arm that was pulled and the 
+        reward that was received.
+        (The value of arm_index is the same as the one returned by give_pull 
+        but set_pulled is the set that is randomly chosen when the pull is 
+        requested from the bandit instance.)
+"""
+
+import numpy as np
+
+# START EDITING HERE
+# You can use this space to define any helper functions that you need
+# END EDITING HERE
+
+
+class MultiBanditsAlgo:
+    def __init__(self, num_arms, horizon):
+        # You can add any other variables you need here
+        self.num_arms = num_arms
+        self.horizon = horizon
+        # START EDITING HERE
+        self.no_of_successes = np.zeros((2, num_arms))
+        self.no_of_failures = np.zeros((2, num_arms))
+        
+        # END EDITING HERE
+    
+    def give_pull(self):
+        # START EDITING HERE
+        beta_sample = np.random.beta(self.no_of_successes+1, self.no_of_failures+1)
+        beta_sample_average = np.mean(beta_sample, axis=0)
+        return np.argmax(beta_sample_average)
+        # raise NotImplementedError
+        # END EDITING HERE
+    
+    def get_reward(self, arm_index, set_pulled, reward):
+        # START EDITING HERE
+        if reward == 1:
+            self.no_of_successes[set_pulled][arm_index] += 1
+        else:
+            self.no_of_failures[set_pulled][arm_index] += 1
+        # raise NotImplementedError
+        # END EDITING HERE
+
